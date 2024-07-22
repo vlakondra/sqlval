96c40880e3f82c4d7279241bf6d489cfcc374663
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-07-22 11:47:40 +0500

    wasm

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
     // object was passed to this function.
     console.log("sqlite3:", sqlite3);
     globalThis.sqlite3 = sqlite3;
-    //sqlite = sqlite3;
+    sqlite = sqlite3; //??
   });
   console.log("SQL", sqlite);
   //import { wasm } from "./store.js";
@@ -24,54 +24,83 @@
   //   // data is an Uint8Array representing an SQLite database file
 
   //   // Execute some sql
-  //   let sqlstr = "CREATE TABLE hello (a int, b char);";
-  //   sqlstr += "INSERT INTO hello VALUES (0, 'hello');";
-  //   sqlstr += "INSERT INTO hello VALUES (1, 'world');";
-  //   db.run(sqlstr); // Run the query without returning anything
 
-  //   var res = db.exec("SELECT * FROM hello");
-  //   console.log(res);
-  //   dbb = db;
   // })();
 
   import CodeMirror from "svelte-codemirror-editor";
   // import { sql } from "/home/vkondra/MyDevs/Svelte/sqlval/node_modules/@codemirror/lang-sql";
   import { sql } from "@codemirror/lang-sql";
-  //node_modules/@codemirror/lang-sql
-  ///home/vkondra/MyDevs/Svelte/sqlval/node_modules/@codemirror/lang-sql
   let value = "select * from hello";
   console.log(sql);
-  // let rs = sql();
-  // console.log("rs", rs);
 
-  // console.log("wasm", $wasm);
-  // let db = null;
-  // $wasm().then((v) => {
-  //   console.log(v);
-  //   let sqlstr = "CREATE TABLE hello (a int, b text);";
-  //   sqlstr += "INSERT INTO hello VALUES (0, 'hello');";
-  //   sqlstr += "INSERT INTO hello VALUES (1, 'world');";
-  //   v.run(sqlstr);
-  //   db = v;
-
-  //   var res = v.exec(value);
-  //   console.log(res);
-  // });
-  // console.log(db, "DB");
   let db;
-  function onclick() {
-    //var res = dbb.exec(value);
-    //console.log(res);
+  async function onclick() {
     console.log(globalThis.sqlite3);
     const capi = globalThis.sqlite3.capi /*C-style API*/,
-      oo = globalThis.sqlite3.oo1; /*high-level OO API*/
+      oo = globalThis.sqlite3.oo1;
+
+    console.log("OO", oo);
     console.log(
       "sqlite3 version",
       capi.sqlite3_libversion(),
       capi.sqlite3_sourceid(),
     );
-    db = new oo.DB("/mydb.sqlite3", "ct");
-    console.log("transient db =", db.filename);
+
+    const dataPromise = fetch("sakila_master.db").then((res) =>
+      res.arrayBuffer(),
+    );
+    const buf = await Promise.all([dataPromise]);
+    console.log("BUF", buf);
+
+    const p = await sqlite.wasm.allocFromTypedArray(buf[0]);
+    const db = new oo.DB();
+
+    console.log("PPP", p, db.pointer);
+
+    const rc = capi.sqlite3_deserialize(
+      db.pointer,
+      "main",
+      p,
+      buf[0].byteLength,
+      buf[0].byteLength,
+      capi.SQLITE_DESERIALIZE_FREEONCLOSE,
+      // Optionally:
+      // | sqlite3.capi.SQLITE_DESERIALIZE_RESIZEABLE
+    );
+    console.log("RC", rc);
+    let res = db.checkRc(rc);
+    console.log(res, db);
+
+    let rR = [];
+    db.exec({
+      sql: "SELECT * FROM actor",
+      rowMode: "object",
+      resultRows: rR,
+    });
+    console.log(rR);
+    // const sqlPromise = initSqlJs({
+    //       locateFile: file => `./${file}`
+    //     });
+
+    //https://willschenk.com/howto/2021/sq_lite_in_the_browser/
+
+    //const db = new SQL.Database(new Uint8Array(buf));
+    // console.log(buf);
+    // db = new oo.DB(new Uint8Array(buf)); //, "ct"); //"/mydb.sqlite3", "ct");
+
+    // console.log("transient db =", db);
+
+    // var stmt = db.prepare("SELECT * FROM actor");
+    // console.log(stmt);
+    // let rR = [];
+    // db.exec({
+    //   sql: "SELECT * FROM actor",
+    //   rowMode: "object",
+    //   resultRows: rR,
+    // });
+
+    //let r = db.exec("SELECT name FROM sqlite_schema WHERE type='table'");
+    //console.log(rR, "R");
   }
 
   function oncreate() {
@@ -80,6 +109,15 @@
     db.exec({
       sql: "CREATE TABLE IF NOT EXISTS t(a,b)",
       // ... numerous other options ...
+    });
+
+    console.log("Query data with exec() using rowMode 'stmt'...");
+    db.exec({
+      sql: "select a from t order by a limit 3",
+      rowMode: "stmt",
+      callback: function (row) {
+        console.log("row ", ++this.counter, "get(0) =", row.get(0));
+      }.bind({ counter: 0 }),
     });
   }
 </script>
